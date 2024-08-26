07aa0c2712782558c390e4bb2f7e7525a82fa136
fgitonga <mfredgitonga@gmail.com>
2024-08-26 18:27:34 +0300

    feat: add fucntions and primitive operators concepts

---
+++ more-or-less.js
@@ -0,0 +1,18 @@
+const more = (arg) => arg + 1
+
+const less = (arg) => arg - 1
+
+const add = (num1, num2) => num1 + num2
+
+const sub = (num1,num2) => num1 - num2
+
+
+// console.log(more(7))
+// console.log(less(7))
+// console.log(add(7,7))
+// console.log(sub(7,5))
+
+// //nesting functions
+// const bodmas = add(more(8),sub(6,3))
+
+// console.log(bodmas)
