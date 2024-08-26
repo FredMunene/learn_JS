9edcba217318c89f3e9ab006e65c773edba78e1c
fgitonga <mfredgitonga@gmail.com>
2024-08-26 19:20:00 +0300

    feat: add feat -function  accesses values and updates values of an object

---
+++ change.js
@@ -0,0 +1,18 @@
+// const sourceObject = {
+//     num: 42,
+//     bool: true,
+//     str: 'some text',
+//     log: console.log,
+// }
+
+// get takes a key and returns the corresponding value from 'sorceObject '
+const get = (obj) =>  sourceObject[obj]
+
+// set takes a key and value, it updates the value of the property an dreturns the value.
+const set = (key, value) => {
+    sourceObject.key = value
+    return sourceObject.key
+}
+
+// console.log(get("num"))
+// console.log(set("str",'happy here'))
