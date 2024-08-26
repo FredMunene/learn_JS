832b0a91a647e8d686d7d98396f8229db92818a1
fgitonga <mfredgitonga@gmail.com>
2024-08-26 18:45:23 +0300

    feat:  get length of array or string

---
+++ returns.js
@@ -0,0 +1,13 @@
+const getLength = (array) => array.length
+
+const id = (num) => num
+
+// function first(value) {
+//     if (Array.isArray(value) || typeof value === 'string') {
+//         return value[(value.length-1)];
+//     } else {
+//         throw  new TypeError('Argument must be array or string');
+//     }
+// }
+// console.log(first([10,20,30]))
+// console.log(getLength([10,20,30]))
