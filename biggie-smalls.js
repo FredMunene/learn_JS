4374ba41a795bee4692fa0e12047206f02e7474b
fgitonga <mfredgitonga@gmail.com>
2024-08-27 11:45:05 +0300

    fix:  smallest  integer value as -infinity

--- biggie-smalls.js
+++ biggie-smalls.js
@@ -1,6 +1,12 @@
-let small = Number.MIN_SAFE_INTEGER
+let small = -Infinity
 
 let biggie = Infinity
 // console.log(biggie === 1.976931348623157e308)
 // console.log(typeof(small))
-console.log(small)
+// console.log(small)
+
+console.log(Number.MAX_VALUE > Number.MAX_SAFE_INTEGER ) // true
+console.log(Infinity > Number.MAX_VALUE) // true
+
+console.log(Number.MIN_SAFE_INTEGER < Number.MIN_VALUE) // true
+console.log(-Infinity < Number.MIN_SAFE_INTEGER) // true
