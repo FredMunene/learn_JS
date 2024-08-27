08f023cc006be00f1c353ae8946c7639f52c5b69
fgitonga <mfredgitonga@gmail.com>
2024-08-27 17:30:39 +0300

    fix : defined set as !undefined

--- is.js
+++ is.js
@@ -7,7 +7,7 @@ is.nan = (num) => num !== num ? true : false;
 is.str =(num) => (typeof num) === 'string' ? true : false;
 is.bool = (num) => (typeof num) === 'boolean' ? true : false;
 is.undef = (num) => (typeof num) === 'undefined' ? true : false;
-is.def = (num) => (typeof num) === 'defined' ? true : false;
+is.def = (num) => (typeof num) !== 'undefined' ? true : false;
 is.arr = (num) => Array.isArray(num);
 // object includes plain objects and objects with null prototypes. Arrays are excluded
 is.obj  = (num) => num !== null && typeof num === 'object' && !Array.isArray(num);
