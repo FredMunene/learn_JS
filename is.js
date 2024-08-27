1f353f7771aebddec1a1571667ccecb50314cb64
fgitonga <mfredgitonga@gmail.com>
2024-08-27 16:28:44 +0300

    fix : format

--- is.js
+++ is.js
@@ -1,18 +1,18 @@
 // is object
-// const is = {}
+const is = {}
 
 
-is.num = (num) => (typeof num) === 'number' ? true : false,
-is.nan = (num) => (typeof num) === 'NaN' ? true : false,
-is.str =(num) => (typeof num) === 'string' ? true : false,
-is.bool = (num) => (typeof num) === 'boolean' ? true : false,
-is.undef = (num) => (typeof num) === 'undefined' ? true : false,
-is.def = (num) => (typeof num) === 'defined' ? true : false,
-is.arr = (num) => (typeof num) === 'array' ? true : false,
-is.obj  = (num) => (typeof num) === 'object' ? true : false,
-is.fun = (num) => (typeof num) === 'fucntion' ? true : false,
-is.truthy = (num) => (typeof num) === 'truthy' ? true : false,
-is.falsy  = (num) => (typeof num) === 'falsy' ? true : false,
+is.num = (num) => (typeof num) === 'number' ? true : false
+is.nan = (num) => (typeof num) === 'NaN' ? true : false
+is.str =(num) => (typeof num) === 'string' ? true : false
+is.bool = (num) => (typeof num) === 'boolean' ? true : false
+is.undef = (num) => (typeof num) === 'undefined' ? true : false
+is.def = (num) => (typeof num) === 'defined' ? true : false
+is.arr = (num) => (typeof num) === 'array' ? true : false
+is.obj  = (num) => (typeof num) === 'object' ? true : false
+is.fun = (num) => (typeof num) === 'fucntion' ? true : false
+is.truthy = (num) => (typeof num) === 'truthy' ? true : false
+is.falsy  = (num) => (typeof num) === 'falsy' ? true : false
 
 
 // console.log(is.num(8))
