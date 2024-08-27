8dd772a3bab3cc2c7ca3e0ccbe5fae44b793d0e2
fgitonga <mfredgitonga@gmail.com>
2024-08-27 17:22:32 +0300

    fix: spelling of function

--- is.js
+++ is.js
@@ -11,12 +11,12 @@ is.def = (num) => (typeof num) === 'defined' ? true : false;
 is.arr = (num) => Array.isArray(num);
 // object includes plain objects and objects with null prototypes. Arrays are excluded
 is.obj  = (num) => num !== null && typeof num === 'object' && !Array.isArray(num);
-is.fun = (num) => (typeof num) === 'fucntion' ? true : false;
+is.fun = (num) => (typeof num) === 'function' ? true : false;
 is.truthy = (num) => (typeof num) === 'truthy' ? true : false;
 is.falsy  = (num) => (typeof num) === 'falsy' ? true : false;
 
 
-console.log(is.obj([{}, { length: 10 }, Object.create(null)]))
+// console.log(is.fun([console.log))
 // console.log(is.nan(NaN))
 
 
