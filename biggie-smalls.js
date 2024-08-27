4d6cf644b9a53383397dfcd8f743d80c9ece9c9c
fgitonga <mfredgitonga@gmail.com>
2024-08-27 11:20:08 +0300

    fix:

--- biggie-smalls.js
+++ biggie-smalls.js
@@ -1,5 +1,6 @@
-let small = Number.MIN_VALUE
+let small = Number.MIN_SAFE_INTEGER
 
 let biggie = Infinity
 // console.log(biggie === 1.976931348623157e308)
 // console.log(typeof(small))
+console.log(small)
