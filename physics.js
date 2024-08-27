3196774f9d49fc54ad8ea0a2cb67fbf7adba61fc
fgitonga <mfredgitonga@gmail.com>
2024-08-27 18:08:47 +0300

    feat:  capture NaN elements

--- physics.js
+++ physics.js
@@ -1,22 +1,26 @@
-const getAcceleration = (answers) => {
+const getAcceleration = (question) => {
 
-    var ans1 = question.f /question.m
-    var ans2 = question.Δv/question.Δt
-    var ans3 = 2*question.d/ (question.t*question.t)
+ 
+    var ans1 = question.f /question.m;
+    var ans2 = question.Δv/question.Δt;
+    var ans3 = 2*question.d/ (question.t*question.t);
+    // isNan captures NotANumber 
+    if (!isNaN(ans1))  return ans1;
+    if (!isNaN(ans2))  return ans2;
+    if (!isNaN(ans3))  return ans3;
 
-  return ( ans1 !== undefined ? ans1 : ans2 !== undefined ? ans2 : ans3 !== undefined ? ans3 : "impossible" )
+     return "impossible"
 
 }
 
-const question = {
-  f: 10,
-  m: 5,
-  Δv: 100,
-  Δt: 50,
-  t:1,
-  d: 10
-}
+// const question = {
+//   f: 10,
+//   m: 5,
+//   Δv: 100,
+//   Δt: 50,
+//   t:1,
+//   d: 10
+// }
   
-console.log(getAcceleration(question))
-
+console.log(getAcceleration({}))
 
