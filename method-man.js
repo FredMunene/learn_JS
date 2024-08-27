2a5435e1bbbc9bf61d5b84b79db9d862a835de4c
fgitonga <mfredgitonga@gmail.com>
2024-08-27 12:45:19 +0300

    fix: added asterick

--- method-man.js
+++ method-man.js
@@ -2,7 +2,7 @@
 const  words = (str) => str.split(" ")
 const sentence = (arry) => arry.join(" ")
 const yell = (str) => str.toUpperCase()
-const whisper = (str) => str.toLowerCase()
+const whisper = (str) => '*'+str.toLowerCase()+'*'
 
 function capitalize (str){
     return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
