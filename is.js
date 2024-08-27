5fd944894e0adeddec60b09d02eeaad200ab805a
fgitonga <mfredgitonga@gmail.com>
2024-08-27 16:21:40 +0300

    fix : object already declared

--- is.js
+++ is.js
@@ -1,18 +1,19 @@
 // is object
+// const is = {}
 
-const is = {
-    num :(num) => (typeof num) === 'number' ? true : false,
-    nan :(num) => (typeof num) === 'Nam' ? true : false,
-    str :(num) => (typeof num) === 'string' ? true : false,
-    bool :(num) => (typeof num) === 'boolean' ? true : false,
-    undef :(num) => (typeof num) === 'undefined' ? true : false,
-    def :(num) => (typeof num) === 'defined' ? true : false,
-    arr :(num) => (typeof num) === 'array' ? true : false,
-    obj :(num) => (typeof num) === 'object' ? true : false,
-    fun :(num) => (typeof num) === 'fucntion' ? true : false,
-    truthy :(num) => (typeof num) === 'truthy' ? true : false,
-    falsy :(num) => (typeof num) === 'falsy' ? true : false,
-}
+
+is.num = (num) => (typeof num) === 'number' ? true : false,
+is.nan = (num) => (typeof num) === 'Nam' ? true : false,
+is.str =(num) => (typeof num) === 'string' ? true : false,
+is.bool = (num) => (typeof num) === 'boolean' ? true : false,
+is.undef = (num) => (typeof num) === 'undefined' ? true : false,
+is.def = (num) => (typeof num) === 'defined' ? true : false,
+is.arr = (num) => (typeof num) === 'array' ? true : false,
+is.obj  = (num) => (typeof num) === 'object' ? true : false,
+is.fun = (num) => (typeof num) === 'fucntion' ? true : false,
+is.truthy = (num) => (typeof num) === 'truthy' ? true : false,
+is.falsy  = (num) => (typeof num) === 'falsy' ? true : false,
+
 
 console.log(is.str(8))
 
