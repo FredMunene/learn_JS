d7aa74ac7b4e04ca70667b9da5420e72e4be200f
fgitonga <mfredgitonga@gmail.com>
2024-08-27 13:08:08 +0300

    fix:  make 0 undefined for isPositive

--- abs.js
+++ abs.js
@@ -1,7 +1,9 @@
 // Ternary functions
-const isPositive = (number) => (number >= 0 ? true : false) 
+const isPositive = (number) => {(number > 0 ? true : false) }
 
-const abs = (number) => (isPositive(number)) ? number : number*-1
+const abs = (number) => { 
+   return (isPositive(number)) ? number : number*-1
+}
 
-console.log(isPositive(1))
-console.log(abs(-Infinity))
+// console.log(isPositive(0))
+// console.log(abs(-0))
