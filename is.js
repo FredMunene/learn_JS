44a66f77f7c1de782c000881aa567d59bc020c4c
fgitonga <mfredgitonga@gmail.com>
2024-08-27 16:55:57 +0300

    fix: arry = object

--- is.js
+++ is.js
@@ -1,5 +1,5 @@
 // is object
-// const is = {}
+const is = {}
 
 
 is.num = (num) => (typeof num) === 'number' ? true : false
@@ -8,13 +8,14 @@ is.str =(num) => (typeof num) === 'string' ? true : false
 is.bool = (num) => (typeof num) === 'boolean' ? true : false
 is.undef = (num) => (typeof num) === 'undefined' ? true : false
 is.def = (num) => (typeof num) === 'defined' ? true : false
-is.arr = (num) => (typeof num) === 'array' ? true : false
+is.arr = (num) => (typeof num) === 'object' ? true : false
 is.obj  = (num) => (typeof num) === 'object' ? true : false
 is.fun = (num) => (typeof num) === 'fucntion' ? true : false
 is.truthy = (num) => (typeof num) === 'truthy' ? true : false
 is.falsy  = (num) => (typeof num) === 'falsy' ? true : false
 
 
-// console.log(is.nan(Number.NaN))
+console.log(is.arr([7,8]))
+console.log(is.nan(NaN))
 
 
