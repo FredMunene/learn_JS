60738547a90e5a6430f0cf4248bcb3a14a504c65
fgitonga <mfredgitonga@gmail.com>
2024-08-27 17:20:36 +0300

    fix : exclude array and null as objects

--- is.js
+++ is.js
@@ -1,21 +1,22 @@
 // is object
-// const is = {}
+const is = {}
 
 
-is.num = (num) => (typeof num) === 'number' ? true : false
-is.nan = (num) => num !== num ? true : false
-is.str =(num) => (typeof num) === 'string' ? true : false
-is.bool = (num) => (typeof num) === 'boolean' ? true : false
-is.undef = (num) => (typeof num) === 'undefined' ? true : false
-is.def = (num) => (typeof num) === 'defined' ? true : false
-is.arr = (num) => Array.isArray(num)
-is.obj  = (num) => (typeof num) === 'object' ? true : false
-is.fun = (num) => (typeof num) === 'fucntion' ? true : false
-is.truthy = (num) => (typeof num) === 'truthy' ? true : false
-is.falsy  = (num) => (typeof num) === 'falsy' ? true : false
+is.num = (num) => (typeof num) === 'number' ? true : false;
+is.nan = (num) => num !== num ? true : false;
+is.str =(num) => (typeof num) === 'string' ? true : false;
+is.bool = (num) => (typeof num) === 'boolean' ? true : false;
+is.undef = (num) => (typeof num) === 'undefined' ? true : false;
+is.def = (num) => (typeof num) === 'defined' ? true : false;
+is.arr = (num) => Array.isArray(num);
+// object includes plain objects and objects with null prototypes. Arrays are excluded
+is.obj  = (num) => num !== null && typeof num === 'object' && !Array.isArray(num);
+is.fun = (num) => (typeof num) === 'fucntion' ? true : false;
+is.truthy = (num) => (typeof num) === 'truthy' ? true : false;
+is.falsy  = (num) => (typeof num) === 'falsy' ? true : false;
 
 
-// console.log(is.arr([[], [1, Array(1), [], 2]]))
+console.log(is.obj([{}, { length: 10 }, Object.create(null)]))
 // console.log(is.nan(NaN))
 
 
