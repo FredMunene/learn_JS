94cd95ab1e24519a98a8b6d91d0d9aa668cb0d47
fgitonga <mfredgitonga@gmail.com>
2024-08-30 13:51:44 +0300

    feat: division,multiply and modulo

---
+++ elementary.js
@@ -0,0 +1,30 @@
+const multiply = (a,b) => {
+    let count = 0
+    let res = 0
+
+    while ( count < b){
+        res+=a
+        count++
+    }
+
+    return res
+}
+
+console.log(multiply(2,3))
+
+const divide = (a,b) => {
+    let count = 0
+
+    while (a >= b){
+        a-=b
+        count++
+    }
+
+    return count
+}
+
+console.log(divide(6,3))
+
+const modulo = (a,b) => a - (divide(a,b)*b)
+
+console.log(modulo(6,4))
