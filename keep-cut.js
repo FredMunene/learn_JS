8dba95ffb2dad5dc6af67818d67351e2e315d8b8
fgitonga <mfredgitonga@gmail.com>
2024-08-31 00:42:53 +0300

    fix : less than 2 length

--- keep-cut.js
+++ keep-cut.js
@@ -1,24 +1,43 @@
 function cutFirst(str){
-    return str.slice(2)
+    if (str.length > 2){
+        return str.slice(2)
+    } 
+    return str
+    
 }
 
 function cutLast(str){
+    if (str.length > 2){
     return str.slice(0,str.length-2)
+    } 
+    return str
 }
 function cutFirstLast(str){
+    if (str.length > 2){
     return str.slice(2,str.length-2)
+    }
+    return str
 }
 
 function keepFirst(str){
+    if (str.length > 2){
     return str.slice(0,2)
+    }
+    return str
 }
 
 function keepLast(str){
+    if (str.length > 2){
     return str.slice(str.length-2)
+    }
+    return str
 }
 
 function keepFirstLast(str){
+    if (str.length > 2){
     return keepFirst(str) + keepLast(str)
+    }
+    return str
 }
 // console.log(cutFirst("abhgcd"))
 // console.log(cutLast("abghcd"))
