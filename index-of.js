f273c3581d65037bf03d977eea211812741772ed
fgitonga <mfredgitonga@gmail.com>
2024-08-30 22:31:22 +0300

    fix: addan optional index

--- index-of.js
+++ index-of.js
@@ -3,13 +3,13 @@ const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
 function indexOf (array, value,val2)  {
     for (const key in array) {
         if (val2 ===  undefined) {
-            if ( array[key] === value) {
+            // if ( array[key] === value) {
+            //     return parseInt(key)  
+            // }
+        } else{
+            if ( key > val2 && array[key] === value) {
                 return parseInt(key)  
             }
-        } else{
-                if ( key >= val2 && array[key] === value) {
-                    return parseInt(key)  
-                }
 
         }
        
@@ -17,18 +17,26 @@ function indexOf (array, value,val2)  {
     return -1
 
 }
-console.log(indexOf(["t", 0, 0,"t"], "t", 1))
+console.log(indexOf(["t", 0, 0,"t"], "t", 2))
 
-function lastIndexOf (array,value){
+function lastIndexOf (array,value, value2){
     let indexes = [];
     for (const key in array) {
-        if (array[key] === value) {
-            indexes.push(parseInt(key))            
+        if (value2 === undefined) {
+            if (array[key] === value) {
+                indexes.push(parseInt(key))            
+            }
+        } else {
+            
+            if (array.slice(value2+1)[key] === value) {
+                indexes.push(parseInt(key))            
+            }
         }
+       
     }
     return indexes[indexes.length-1]
 }
-// console.log(lastIndexOf(beasts,"bison"))
+console.log(lastIndexOf(["t", 0, 0, "t"],"t", 2))
 
 function includes (array,value){
     for (const key in array) {
