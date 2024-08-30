164caa2668b198d70f202d04bad2e88c7c950c63
fgitonga <mfredgitonga@gmail.com>
2024-08-31 02:43:05 +0300

    feat: revearse array or string

---
+++ reverse.js
@@ -0,0 +1,25 @@
+function reverse (str){
+    let res = Array.isArray(str) ? [] : ''
+
+    for ( let i = str.length - 1; i >= 0 ;i++){
+        if (Array.isArray(str)){
+            res.push(str[i])
+        } else {
+            res += str[i]
+        }
+    }
+
+    return res
+}
+
+const array1 = ['one', 'two', 'three'];
+console.log('array1:', array1);
+// Expected output: "array1:" Array ["one", "two", "three"]
+
+const reversed = array1.reverse();
+console.log('reversed:', reversed);
+// Expected output: "reversed:" Array ["three", "two", "one"]
+
+// Careful: reverse is destructive -- it changes the original array.
+console.log('array1:', array1);
+// Expected output: "array1:" Array ["three", "two", "one"]
