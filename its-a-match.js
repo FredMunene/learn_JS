d40ed7ae3f35905d24c33689fcee108b76d42506
fgitonga <mfredgitonga@gmail.com>
2024-08-31 16:47:00 +0300

    feat: regex

---
+++ its-a-match.js
@@ -0,0 +1,15 @@
+function grep(){
+    // ^ first character
+    // $ last character
+    // () include this in match
+    // | OR
+    //
+}
+
+const normal = /hi/
+
+const begin = /^hi/
+
+const end = /hi$/
+
+const beginEnd = /^hi$/
