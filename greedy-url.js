5a89eaba01d5328b8ee43389e0595e186d51189e
Fred Gitonga <fgitonga@noreply.learn.zone01kisumu.ke>
2024-08-31 17:18:09 +0000

    Update 'greedy-url.js'

--- greedy-url.js
+++ greedy-url.js
@@ -7,3 +7,19 @@ function getURL(str) {
     return y;
 }
 
+
+function greedyQuery(data) {
+    const regex = /https?:\/\/[^\s?]+(\?\S+)?/g;
+    return (data.match(regex) || []).filter((url) => {
+        const que = url.split(/[&?]/).length - 1;
+        return que >= 3;
+    });
+}
+
+function notSoGreedy(data) {
+    const regex = /https?:\/\/[^\s?]+(\?\S+)?/g;
+    return (data.match(regex) || []).filter((url) => {
+        const que = url.split(/[&?]/).length - 1;
+        return que >= 2 && que < 4;
+    });
+}
