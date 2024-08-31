9e25f87ce4be8d9775a6082295529299b046d2ba
fgitonga <mfredgitonga@gmail.com>
2024-08-31 03:37:30 +0300

    fix: consider multiple chars

--- unbreakable.js
+++ unbreakable.js
@@ -1,24 +1,25 @@
 function split(str, del) {
-    let newstr = [];
-    if (del === ''){
-
+    const array = [];
+    
+    if (del === '') {
+        // When the del is an empty str, split the str into individual characters
+        for (let i = 0; i < str.length; i++) {
+            array.push(str[i]);
+        }
     } else {
-        let chunky = '';
-
-
-        for (let i = 0; i < str.length;i++){
-            if (str.slice(i, i + del.length) === del){
-                newstr.push(chunky);
-                chunky = '';
-                i += del.length -1;
+        let slice = '';
+        for (let i = 0; i < str.length; i++) {
+            if (str.slice(i, i + del.length) === del) {
+                array.push(slice);
+                slice = '';
+                i += del.length - 1;
             } else {
-                chunky += str[i];
+                slice += str[i];
             }
         }
-     newstr.push(chunky)
+        array.push(slice); // Push the last chunk
     }
-    return newstr
-
+    return array;
 }
 
 function join(array, del =','){
