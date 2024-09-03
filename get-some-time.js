e12530f9525b25277a5bae875a97e505532a49e4
fgitonga <mfredgitonga@gmail.com>
2024-09-03 21:59:59 +0300

    fix:

--- get-some-time.js
+++ get-some-time.js
@@ -20,12 +20,12 @@ function firstDayWeek(week, year) {
 
 function getWeekDay(date) {
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    return days[date.getDay()];
+    return days[date.getDay() - 1];
 }
 
 function formatDate(date) {
     let month = String(date.getMonth() + 1).padStart(2, '0');
-    let day = String(date.getDate()).padStart(2, '0');
+    let day = String(date.getDate() - 1).padStart(2, '0');
     let year = String(date.getFullYear()).padStart(4, '0');
     return `${day}-${month}-${year}`;
 }
