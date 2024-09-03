2cf143538e486d65d94003b28b1e887defe0a2ea
fgitonga <mfredgitonga@gmail.com>
2024-09-03 14:39:25 +0300

    fix: validate time

--- date-is.js
+++ date-is.js
@@ -1,8 +1,7 @@
-//  Common JS : import statement
-// const {isValid} = require('date-fns/isValid')
 
-//ES modules : import statement
-import {isValid} from 'data-fns';
+function isValid(date){
+    return !isNaN(date.getTime())
+}
 
 function isAfter(date1,date2){
     return date1.getTime() > date2.getTime()
@@ -19,17 +18,18 @@ function isPast(date){
     return isValid(date) && date.getTime() < Date.now()
 }
 
-const date = new Date('2020-05-29 23:25:22');
-console.log(isValid(date)); // true if the date is valid
-
-const invalidDate = new Date('Invalid Date String');
-console.log(isValid(invalidDate)); //
-
 // Testing the functions
-const futureDate = new Date('2030-01-01');
-const pastDate = new Date('2010-01-01');
 
-console.log(isAfter(futureDate, pastDate)); // true
-console.log(isBefore(futureDate, pastDate)); // false
-console.log(isFuture(futureDate)); // true
-console.log(isPast(pastDate)); // true
+// const date = new Date('2020-05-29 23:25:22');
+// console.log(isValid(date)); // true if the date is valid
+
+// const invalidDate = new Date('Invalid Date String');
+// console.log(isValid(invalidDate)); //
+
+// const futureDate = new Date('2030-01-01');
+// const pastDate = new Date('2010-01-01');
+
+// console.log(isAfter(futureDate, pastDate)); // true
+// console.log(isBefore(futureDate, pastDate)); // false
+// console.log(isFuture(futureDate)); // true
+// console.log(isPast(pastDate)); // true
