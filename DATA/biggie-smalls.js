2730282296c0c4c5456933d9fbfab380f05c7421
fgitonga <mfredgitonga@gmail.com>
2024-09-04 09:19:08 +0300

    build: add files to folders

--- DATA/biggie-smalls.js
+++ DATA/biggie-smalls.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 let small = -Infinity
-=======
-var smalls = -Infinity;
->>>>>>> 51da101b8cf2634b5fb6daffa05d351c7f41ea8a
 
+// var smalls = -Infinity;
 var biggie = Infinity;
 
 // console.log(Number.MAX_VALUE > Number.MAX_SAFE_INTEGER ) // true
