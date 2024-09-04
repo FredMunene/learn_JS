39662174456bbaa1c414c8d7634a5a9111b17218
fgitonga <mfredgitonga@gmail.com>
2024-09-04 22:48:35 +0300

    feat: array.some and array.every

---
+++ long-words.js
@@ -0,0 +1,19 @@
+function longWords(array){
+    return array.every(
+        str => typeof str === 'string'&& str.length >= 5 
+    );
+
+}
+
+function OneLongWords(array){
+    return array.some(
+    str => typeof str === 'string' && str.length >= 10
+);
+    
+}
+
+function noLongWords(array){
+    return array.every(
+        str => typeof str !== 'string' && str.length < 7
+    );
+}
