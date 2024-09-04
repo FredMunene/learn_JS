ca06f834ac678811d09ed81b291919dcf82bb627
fgitonga <mfredgitonga@gmail.com>
2024-09-04 23:19:31 +0300

    fix:  callback second parameter as index

--- for-each.js
+++ for-each.js
@@ -1,5 +1,9 @@
 function forEach(array,func) {
-    for (var element of array){
-        func(element);
+    for (var index = 0; index < array.length;index++){
+        func(array[index],index,array);
     }
 }
+
+const numbers = [1, 2, 3, 4, 5];
+
+forEach(numbers,console.log)
