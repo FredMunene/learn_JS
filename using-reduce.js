285cb2a8e290d35cc9226faab5ee8f26c66e7fc9
fgitonga <mfredgitonga@gmail.com>
2024-09-04 22:37:14 +0300

    fix: set optional parameter to 0

--- using-reduce.js
+++ using-reduce.js
@@ -1,18 +1,18 @@
 // reduce
-const adder = (array,num) => array.reduce(
+const adder = (array,num = 0) => array.reduce(
     (total, number) => total + number,
     num   
 )
 const array1 = [1, 2, 3, 4];
-console.log(adder(array1,6));
+console.log(adder([1, 2, 3, 4],10));
 
-const sumOrMul = (array,num) => array.reduce(
+const sumOrMul = (array,num = 0) => array.reduce(
     (total, number) => number % 2 === 0 ? total*number : total+number,
     num    
 )
 
-console.log(sumOrMul(array1,5));
+// console.log(sumOrMul(array1,5));
 
-const funcExec = (functions, num) => functions.reduce(
+const funcExec = (functions, num = 0) => functions.reduce(
     (result, func) => func(result),num
 ) 
