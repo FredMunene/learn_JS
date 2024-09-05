6a0cd3f8bbfd3c788f846f53096372cc90efdaa9
fgitonga <mfredgitonga@gmail.com>
2024-09-05 15:14:05 +0300

    feat: .filter, .reject, .partition

--- filter.js
+++ filter.js
@@ -1,9 +1,40 @@
 function filter(array,func){
-
+    const truthy = [];
+    for (let index = 0; index < array.length;index++){
+        if (func(array[index],index,array)){
+            result.push(array[index])
+        }
+    }
+    return truthy;
 }
 function reject(array,func){
+    const falsey = [];
+    for (let index = 0; index < array.length;index++){
+        if (!func(array[index],index,array)){
+            result.push(array[index])
+        }
+    }
+    return falsey;
 
 }
 function partition(array,func){
+    const truthy = [];
+    const falsey = [];
+    for (let index = 0; index < array.length;index++){
+        if (func(array[index],index,array)){
+            truthy.push(array[index])
+        } else {
+            falsey.push(array[index])
+        }
+    }
+    return [truthy,falsey];
 
 }
+
+var users = [
+    { 'user': 'barney',  'age': 36, 'active': false },
+    { 'user': 'fred',    'age': 40, 'active': true },
+    { 'user': 'pebbles', 'age': 1,  'active': false }
+  ];
+
+console.log(partition(users, num => num.active === true))
