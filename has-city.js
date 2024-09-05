9291e41011a02099875624943d2f4fdc4be509f9
fgitonga <mfredgitonga@gmail.com>
2024-09-05 21:33:45 +0300

    feat: curry

---
+++ has-city.js
@@ -0,0 +1,11 @@
+function hasCity(country, array) {
+    return function(city){
+        if (array.includes(city)){
+            return `${city} is a city from ${country}.`
+        } else{
+            return `${city} is not a city from ${country}.`
+        }
+    }
+}
+
+console.log(hasCity('Kenya',['Nai','Kis'])('Nali'))
