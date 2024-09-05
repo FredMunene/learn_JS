45619e9b9aec78f4e678268373d4b810bb725bf4
fgitonga <mfredgitonga@gmail.com>
2024-09-05 22:28:16 +0300

    feat:

---
+++ currify.js
@@ -0,0 +1,22 @@
+function currify(func){
+    return function curried(...args){// rest parameter to collect arguments
+        // if the number of arguments provided is suffient to call with original function
+        if (args.length >= func.length){
+            // calls original function with all arguments
+            return func(...args);
+        } else {
+            return function(...nextArgs){
+                // call recursiveely the currify function
+                return curried(...args, ...nextArgs);
+            };
+        }
+    };
+}
+
+const mult2 = (el1, el2) => el1 * el2
+console.log(mult2(2, 2)) // result expected 4
+
+const mult2Curried = currify(mult2)
+
+console.log(mult2Curried(2)(2)) // result expected 4
+// (same result, with a function that has technically only one argument)
