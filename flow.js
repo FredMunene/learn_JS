cdd08be682e1eb8b654c112239e5703e8c77f4a3
fgitonga <mfredgitonga@gmail.com>
2024-09-05 21:55:59 +0300

    fix: comment example

--- flow.js
+++ flow.js
@@ -24,8 +24,8 @@ function flow(funcs) {
 
 
 
-const square = (nbr) => nbr * nbr
-const add2Numbers = (nbr1, nbr2) => nbr1 + nbr2
+// const square = (nbr) => nbr * nbr
+// const add2Numbers = (nbr1, nbr2) => nbr1 + nbr2
 
-const flowedFunctions = flow([add2Numbers, square])
-console.log(flowedFunctions(2, 3)) // -> 25
+// const flowedFunctions = flow([add2Numbers, square])
+// console.log(flowedFunctions(2, 3)) // -> 25
