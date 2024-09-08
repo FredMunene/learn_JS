7033d29f9b7e4487e525d0b555f0665a6bba47cb
fgitonga <munene2050gitonga@gmail.com>
2024-09-08 13:00:24 +0300

    build: move files to DOM

---
+++ DOM-element-manipulation.md
@@ -0,0 +1,6 @@
+// id = person_name  | tag = a or span | class = classe | active = class
+
+1. getElementsByTagName() // a , span, p
+2. getElementByClassName()
+
+nodeList vs Array
---
+++ DOM/pick-and-click/pick-and-click.js
@@ -0,0 +1,9 @@
+
+export function pick (){
+    //turn screen to a hsl color picker.
+    // varies the hue and luminosity according to position of the mouse
+
+    // background color of body to change based on position of mouse
+    // X vary hue value 0-360
+    // Y axis vary luminosity 1-100
+}
