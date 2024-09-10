074e79665bdc38e23e1a1e963e7628ba597f430c
fgitonga <munene2050gitonga@gmail.com>
2024-09-10 11:21:36 +0300

    feat:  modifying an object

---
+++ pick-omit.js
@@ -0,0 +1,40 @@
+function pick(obj,array) {
+    const newObj  = {}
+
+    if (typeof array === 'string'){
+      array = array.split();
+    }
+    
+    for (let value of array){
+        if (obj[value]){
+            newObj[value] = obj[value]
+        }
+    }
+
+    return newObj
+
+    //return an object whose keys are present in the string or array
+
+}
+
+function omit(obj,array){
+    // return an object whose key are not in the string
+    const newObj  = {}
+
+    if (typeof array === 'string'){
+        array = array.split()
+    }
+
+    const keys  = Object.keys(obj)
+
+    for (let key of keys){
+        if (!array.includes(key) && obj[key]) {
+            newObj[key] = obj[key]
+        }
+    }
+
+    return newObj
+}
+
+console.log(pick({'ape': 'legend','milk' : 34},['ape','legend','milk']))
+console.log(omit({'ape': 'legend','milk' : 34},'milk'))
